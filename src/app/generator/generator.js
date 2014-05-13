ab188581fb18821eeea158972dac7552c7912a0a
Dexter Gramfors <dexter.gramfors@gmail.com>
2014-05-13 09:17:15 +0200

    Added autofocus of the input field when a square is selected. Also added basic visual feedback when a square is selected.

--- src/app/generator/generator.js
+++ src/app/generator/generator.js
@@ -3,6 +3,7 @@ angular.module('generator', [])
 .controller('mainController', function($scope){
 	$scope.entries = [];
 	$scope.currentIndex = 0;
+	$scope.focusInput = true;
 
 	$scope.nextSquare = function() {
 		$scope.currentIndex++;
@@ -12,9 +13,23 @@ angular.module('generator', [])
 	$scope.selectSquare = function(index) {
 		$scope.bingoInput = $scope.entries[index];
 		$scope.currentIndex = index;
+		$scope.focusInput = true;
 	}
 
 	$scope.$watch('bingoInput', function(newVal) {
 		$scope.entries[$scope.currentIndex] = newVal;
 	})
+})
+
+.directive('focusThis', function() {
+	return {
+		link: function(scope, element, attrs) {
+			scope.$watch(attrs.focusThis, function(value) {
+				if (value === true) {
+					element[0].focus();
+					scope[attrs.focusThis] = false;
+				};
+			})
+		}
+	}
 });
